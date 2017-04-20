a51ce32fc9faded7c1b827d9c21bd9ea517a67c0
Reo Dongmin Lee <reoim10@gmail.com>
2017-04-20 17:34:34 +0900

    variables

---
+++ variables/shorthand.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 3.14
+	d := true
+
+	fmt.Printf("%v \t %T \n", a, a)
+	fmt.Printf("%v \t %T \n", b, b)
+	fmt.Printf("%v \t %T \n", c, c)
+	fmt.Printf("%v \t %T \n", d, c)
+}
---
+++ variables/varzerovalue.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Printf("%v \t %T \n", a, a)
+	fmt.Printf("%v \t %T \n", b, b)
+	fmt.Printf("%v \t %T \n", c, c)
+	fmt.Printf("%v \t %T \n", d, d)
+}
